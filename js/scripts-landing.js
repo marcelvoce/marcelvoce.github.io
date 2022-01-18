859c87f7c73aa76d8b19aa78a0e4410e743a5e01
Cosminn <petregcosmin@gmail.com>
2022-01-18 12:22:43 +0200

    Last changes

--- js/scripts-landing.js
+++ js/scripts-landing.js
@@ -1,6 +1,6 @@
 /* Back To Top Button */
 // create the back to top button
-$('body').prepend('<div><a href="#contact" class="register-now page-scroll"><div class="logox"><img class="bell" src="images/alarm.png" style="height: 32px"></div>Inregistreaza-te</a></div>');
+$('body').prepend('<div><a href="#contact" class="register-now page-scroll"><div class="logox"><img class="bell" src="images/alarm.png" style="height: 32px"></div>Inregistreaza-te&nbsp;</a></div>');
 var amountScrolled = 700;
 var amountScrolledBottom = 2800;
 $(window).scroll(function () {
