e775335ea4e8c44885dc82d5ff4da28041dfadb8
Cosminn <petregcosmin@gmail.com>
2022-01-24 00:57:25 +0200

    font size

--- js/scripts-landing.js
+++ js/scripts-landing.js
@@ -1,6 +1,6 @@
 /* Back To Top Button */
 // create the back to top button
-$('body').prepend('<div><a href="#contact" class="register-now page-scroll" style="padding-right:10px"><div class="logox"><img class="bell" src="images/alarm.png" style="height: 28px"></div>Inregistreaza-te</a></div>');
+$('body').prepend('<div><a href="#contact" class="register-now page-scroll" style="padding-right:10px; font-size:20px"><div class="logox"><img class="bell" src="images/alarm.png" style="height: 28px"></div>Inregistreaza-te</a></div>');
 var amountScrolled = 700;
 var amountScrolledBottom = 2800;
 $(window).scroll(function () {
